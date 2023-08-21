c899b7ab22d0b0770b363afce1a4edfcb8f3f9af
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2023-08-21 08:36:16 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirnam, 'inventory.sqlite');
+const dbPath = path.resolve('inventory.sqlite');
 const db = new sqlite3.Database(dbPath, (err) => {
   if(err){
     console.error('Database Connection error: ', err.message);
